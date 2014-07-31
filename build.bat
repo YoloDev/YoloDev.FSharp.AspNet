5376030f5ac99d2cde1799a70df7563ce56c29e2
Alxandr <alxandr@alxandr.me>
2014-07-31 10:40:23 +0000

    Initial commit

---
+++ build.bat
@@ -0,0 +1,26 @@
+@echo off
+cd %~dp0
+
+SETLOCAL
+SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
+
+IF EXIST %CACHED_NUGET% goto copynuget
+echo Downloading latest version of NuGet.exe...
+IF NOT EXIST %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '%CACHED_NUGET%'"
+
+:copynuget
+IF EXIST .nuget\nuget.exe goto restore
+md .nuget
+copy %CACHED_NUGET% .nuget\nuget.exe > nul
+
+:restore
+.nuget\NuGet.exe install FSharpSupport -ExcludeVersion -o packages -nocache -pre
+.nuget\NuGet.exe install KoreBuild -ExcludeVersion -o packages -nocache -pre
+
+IF "%SKIP_KRE_INSTALL%"=="1" goto run
+CALL packages\KoreBuild\build\kvm upgrade -svr50 -x86
+CALL packages\KoreBuild\build\kvm install default -svrc50 -x86
+
+:run
+echo DONE
---
+++ src/FSharpSupport/provider.fs
@@ -0,0 +1,188 @@
+namespace FSharpSupport
+
+open System.IO
+open System.Runtime.Versioning
+open System.Collections.Generic
+open Microsoft.Framework.Runtime
+open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.SimpleSourceCodeServices
+
+module Helpers =
+    let inline ni () = raise (new System.NotImplementedException ())
+
+open Helpers
+
+type internal FSharpProjectReference(project: Project, targetFramework: FrameworkName, configuration: string, projectExport: ILibraryExport, watcher: IFileWatcher) =
+    
+    let param name value = sprintf "--%s:%s" name value
+
+    let emit outputPath emitPdb emitDocFile emitExe inMemory =
+        //System.Diagnostics.Debugger.Launch () |> ignore
+        let tempBasePath = Path.Combine [|outputPath; project.Name; "obj"|]
+
+        let convertRef (r: IMetadataReference) =
+            match r with
+            // Skip this project
+            | _ when r.Name = (typeof<FSharpProjectReference>.Assembly.GetName ()).Name -> None, None
+
+            // NuGet references
+            | :? IMetadataFileReference as r -> Some r.Path, None
+
+            // Assembly neutral references
+            | :? IMetadataEmbeddedReference as r ->
+                let tempEmbeddedPath = Path.Combine [|tempBasePath; r.Name + ".dll"|]
+                Directory.CreateDirectory (Path.GetDirectoryName tempEmbeddedPath) |> ignore
+
+                // Write the ANI to disk
+                File.WriteAllBytes (tempEmbeddedPath, r.Contents)
+                Some tempEmbeddedPath, Some tempEmbeddedPath
+
+            // Project references
+            | :? IMetadataProjectReference as r ->
+                let path = 
+                    let path = Path.Combine [|tempBasePath; r.Name + ".dll"|]
+                    Directory.CreateDirectory (Path.GetDirectoryName path) |> ignore
+
+                    // Write metadata to disk
+                    use fs = File.OpenWrite path
+                    r.EmitReferenceAssembly fs
+                    path
+
+                Some path, Some path
+
+            | _ -> None, None
+
+
+        let outExt ext = Path.Combine [|outputPath; project.Name + ext|]
+        let outputDll = outExt (if emitExe then ".exe" else ".dll")
+
+        let compilerArgs = [(param "out" outputDll); "--target:" + (if emitExe then "exe" else "library"); "--noframework"]
+        
+        if not inMemory then
+            Directory.CreateDirectory tempBasePath |> ignore
+
+        let compilerArgs = 
+            match emitPdb with
+            | false -> compilerArgs
+            | true ->
+                let pdb = outExt ".pdb"
+                (param "pdb" pdb) :: "--debug" :: compilerArgs
+
+        let compilerArgs =
+            match emitDocFile with
+            | false -> compilerArgs
+            | true ->
+                let doc = outExt ".xml"
+                (param "doc" doc) :: compilerArgs
+
+        // F# cares about order so assume that the files were listed in order
+        let compilerArgs = (project.SourceFiles |> List.ofSeq |> List.rev) @ compilerArgs
+        project.SourceFiles
+        |> Seq.iter (fun f -> watcher.WatchFile f |> ignore)
+
+        // These are the metadata references being used by your project.
+        // Everything in your project.json is resolved and normailzed here:
+        // - Project references
+        // - Package references are turned into the appropriate assemblies
+        // - Assembly neutral references
+        // Each IMetadaReference maps to an assembly
+        let compilerArgs, tempFiles =
+            let refs, tempFiles =
+                projectExport.MetadataReferences
+                |> List.ofSeq
+                |> List.map convertRef
+                |> List.unzip
+
+            let refs = refs |> List.choose id
+            let tempFiles = tempFiles |> List.choose id
+
+            let rec appender refs acc =
+                match refs with
+                | [] -> List.rev acc
+                | h :: t -> appender t ("-r" :: h :: acc)
+
+            let refs = appender refs []
+            refs @ compilerArgs, tempFiles
+        
+        let compilerArgsArr = ("fsc.exe" :: (compilerArgs |> List.rev)) |> Array.ofList
+
+        let scs = SimpleSourceCodeServices()
+        let errors, exitCode = scs.Compile compilerArgsArr
+        tempFiles |> Seq.iter (fun f -> File.Delete f |> ignore)
+        Directory.Delete tempBasePath
+        let warnings =
+            errors
+            |> Array.choose (fun e ->
+                match e.Severity with
+                | Severity.Warning -> Some e
+                | _ -> None)
+            |> Array.map (fun e -> e.ToString ())
+        
+        let errors =
+            errors
+            |> Array.choose (fun e ->
+                match e.Severity with
+                | Severity.Error -> Some e
+                | _ -> None)
+            |> Array.map (fun e -> e.ToString ())
+
+        new DiagnosticResult (true, warnings, errors) :> IDiagnosticResult
+
+    interface IMetadataReference with
+        member x.Name with get () = project.Name
+    
+    interface IMetadataProjectReference with
+        member x.ProjectPath with get () = project.ProjectFilePath
+    
+        member x.GetDiagnostics () =
+            let outputDir = Path.Combine [|Path.GetTempPath (); "diagnostics-" + (System.Guid.NewGuid ()).ToString ()|]
+
+            try
+                emit outputDir false false false true
+            finally
+                Directory.Delete (outputDir, true)
+
+        member x.Load (loaderEngine) =
+            let outputDir = Path.Combine [|Path.GetTempPath (); "dynamic-assemblies"|]
+
+            let result = emit outputDir true false false true
+
+            match result.Success with
+            | false -> raise (new CompilationException (result.Errors |> System.Linq.Enumerable.ToList))
+            | true ->
+                let assemblyPath = Path.Combine [|outputDir; project.Name + ".dll"|]
+
+                loaderEngine.LoadFile assemblyPath
+
+        member x.EmitReferenceAssembly (stream) =
+            let outputDir = Path.Combine [|Path.GetTempPath (); "reference-assembly-" + (System.Guid.NewGuid ()).ToString ()|]
+
+            try
+                let result = emit outputDir false false false false
+
+                match result.Success with
+                | false -> ()
+                | true ->
+                    use fs = File.OpenRead (Path.Combine [|outputDir; project.Name + ".dll"|])
+                    fs.CopyTo stream
+                    ()
+            
+            finally
+                Directory.Delete (outputDir, true)
+
+        member x.EmitAssembly (path) =
+            emit path true true false false
+
+        member x.GetSources () =
+            project.SourceFiles
+            |> List.ofSeq
+            |> List.map (fun p -> new SourceFileReference (p) :> ISourceReference)
+            |> Array.ofList
+            :> IList<ISourceReference>
+
+type public FSharpProjectReferenceProvider(watcher: IFileWatcher) =
+
+    interface IProjectReferenceProvider with
+
+        member x.GetProjectReference (project, targetFramework, configuration, projectExport) =
+            new FSharpProjectReference (project, targetFramework, configuration, projectExport, watcher) :> IMetadataProjectReference
